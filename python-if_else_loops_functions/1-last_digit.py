dc3f34c7ad5598176bc1a011ec9a6ed6fec1bb90
VandevilleF <flo.vandeville@gmail.com>
2024-01-22 16:30:11 +0100

    Modifying condtion if number is smaller than 0

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-1000, 1000)
-last_d = abs(number) % 10
+
+print(f"Last digit of {number} is ", end="")
 
 if number < 0:
-    last_d = last_d * -1
-
-if last_d > 5:
-    print(f"Last digit of {number} is {last_d} and is greater than 5")
-elif last_d == 0:
-    print(f"Last digit of {number} is {last_d} and is 0")
+    last_digit = number % -10
 else:
-    print(f"Last digit of {number} is {last_d} and is less than 6 and not 0")
+    last_digit = number % 10
+
+if last_digit > 5:
+    print(f"{last_digit} and is greater than 5")
+elif last_digit == 0:
+    print(f"{last_digit} and is 0")
+else:
+    print(f"{last_digit} and is less than 6 and not 0")
