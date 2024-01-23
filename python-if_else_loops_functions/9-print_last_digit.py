3a958b5a33809de4321a59808583fe072dbac051
VandevilleF <flo.vandeville@gmail.com>
2024-01-23 13:10:59 +0100

    Add 9-print_last_digit.py for task 9

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def print_last_digit(number):
+    """
+    Prints the last digit of a number
+
+    Parameters:
+    - number (int): Number whose the last digitmust be returned
+
+    Returns:
+    int: Value of the last digit
+
+    """
+    last_digit = abs(number) % 10
+    print(last_digit, end='')
+    return last_digit
