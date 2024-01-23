86ade1c7651b08c5ac5c9a421d0ba8ec994c81df
VandevilleF <flo.vandeville@gmail.com>
2024-01-23 13:27:54 +0100

    Add 11-pow.py for task 11

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """
+    Computes a to the power of b and return the value.
+
+    Parameters:
+    - a (int): First integer to compute
+    - b (int): Seconde integer to compute
+
+    Returns:
+    int: the value of a to the power of b
+
+    """
+    return a ** b
