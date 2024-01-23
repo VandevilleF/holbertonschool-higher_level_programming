dc4114fc0861d2955a18e11d324d303b52e22d05
VandevilleF <flo.vandeville@gmail.com>
2024-01-23 13:20:12 +0100

    Add 10-add.py for task 10

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+    Adds two integers and returns the result.
+
+    Parameters:
+    - a (int): First integer to add
+    - b (int): Seconde integer to add
+
+    Returns:
+    int: the value of a + b
+
+    """
+    return a + b
