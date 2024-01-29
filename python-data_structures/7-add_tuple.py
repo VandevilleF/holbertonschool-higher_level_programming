6281bae5f79a83323e6f6dd0c79137a1b925cde1
VandevilleF <flo.vandeville@gmail.com>
2024-01-29 16:52:21 +0100

    Add 7-add_tuple.py for task 7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_result = []
+    for i in range (2):
+        value_tup_a = 0 if len(tuple_a) <= i else tuple_a[i]
+        value_tup_b = 0 if len(tuple_b) <= i else tuple_b[i]
+        tuple_result.append(value_tup_a + value_tup_b)
+
+    return tuple(tuple_result)
