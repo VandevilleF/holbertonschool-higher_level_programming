bb290f704bab8a2786c7ea0cc5e88f6759f808b0
VandevilleF <flo.vandeville@gmail.com>
2024-01-29 10:53:48 +0100

    Add block of comment

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """
+    Prints all integers of a list
+
+    Parameters:
+    - my_list (lists): List from which integers are printed
+
+    Returns:
+    int: Integers of the list, one per line
+
+    """
     for i in my_list:
         print("{}".format(i))
