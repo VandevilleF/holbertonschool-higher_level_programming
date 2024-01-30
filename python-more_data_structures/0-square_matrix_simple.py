21eb77845584cc83826084ee80055be37146858c
VandevilleF <flo.vandeville@gmail.com>
2024-01-30 11:26:37 +0100

    pycodestyle line 3 missing whitespace around operator, line 5 and line 7 whitespace

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_square = [[0] *len(row) for row in matrix]
+    matrix_square = [[0] * len(row) for row in matrix]
 
-    for i in range (len(matrix)):
-        for j in range (len(matrix[i])):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
             matrix_square[i][j] = pow(matrix[i][j], 2)
 
     return matrix_square
