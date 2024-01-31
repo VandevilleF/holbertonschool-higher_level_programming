7f6fe54177769146e0e1ce9162b0db313e96a608
VandevilleF <flo.vandeville@gmail.com>
2024-01-31 14:43:59 +0100

    Add condition except IndexError raise

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,6 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             count += 1
+        except IndexError:
+            raise
         except Exception as e:
             pass
     print()
