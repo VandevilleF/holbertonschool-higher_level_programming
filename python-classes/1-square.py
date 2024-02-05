aa30b44692943b6711c24dc322abe5e41d6a92e0
VandevilleF <flo.vandeville@gmail.com>
2024-02-05 13:47:09 +0100

    Add 1-square.py for task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A class that defines a 'square'
+"""
+
+
+class Square:
+    """
+    A class named 'Square'
+    """
+    def __init__(self, size):
+
+        self.__size = size
