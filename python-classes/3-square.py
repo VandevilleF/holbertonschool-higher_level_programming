7c51b4ff94b403fee8e3c747a22551cebd539b58
VandevilleF <flo.vandeville@gmail.com>
2024-02-05 14:34:20 +0100

    Add 3-square.py for task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A class that defines a 'square'
+"""
+
+
+class Square:
+    """
+    A class named 'Square'
+    """
+    def __init__(self, size=0):
+
+        self.__size = size
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+
+        return self.__size * self.__size
