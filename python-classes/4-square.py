dcf2d49c8f7195e67f8cd8ef2bf9c4483e0264df
VandevilleF <flo.vandeville@gmail.com>
2024-02-05 15:43:08 +0100

    Add 4-square.py for task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+A class that defines a 'square'
+"""
+
+
+class Square:
+    """
+    A class named 'Square'
+    """
+    def __init__(self, size=0):
+
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
