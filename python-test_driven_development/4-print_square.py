6c9bac0889e5e0905c948b0c95d4d4c5b779638e
VandevilleF <flo.vandeville@gmail.com>
2024-02-06 21:45:38 +0100

    Add 4-print_square.py and tests/4-print_square.txt for task 3

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Prints a square with the character #
+size is the size length of the square
+size must be an integer,
+if size is less than 0, raise a ValueError
+if size is a float and is less than 0, raise a TypeError"""
+
+
+def print_square(size):
+    """
+    Prints a square with the character #
+    """
+
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
