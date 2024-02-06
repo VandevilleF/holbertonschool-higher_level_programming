7e261f4f148a77b01a2ae33a6a49eb969fefea75
VandevilleF <flo.vandeville@gmail.com>
2024-02-06 19:57:42 +0100

    Pycodestyle line 8 expected 2 blank lines, found 1

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,6 +5,7 @@ a and b must be first casted to integers if they are float
 Returns an integer: the addition of a and b
 """
 
+
 def add_integer(a, b=98):
     """Return an integer
     of the sum of a and b
