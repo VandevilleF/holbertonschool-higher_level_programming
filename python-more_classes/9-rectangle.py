48560462e8f81ad11eddbef61c3cb5e532ed744e
VandevilleF <flo.vandeville@gmail.com>
2024-02-09 11:04:06 +0100

    inverse heigt and width for test width before heigt

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -17,17 +17,18 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
 
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
 
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = width
         Rectangle.number_of_instances += 1
 
     @property
