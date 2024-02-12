f100a2116c52ffbdb2d4b6b3d9a0c8f265cd54e6
VandevilleF <flo.vandeville@gmail.com>
2024-02-12 17:56:08 +0100

    Add 1-my_list.py and tests/1-my_list.txt for task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+class MyList that inherits from list
+
+"""
+
+class MyList(list):
+    """
+    class MyList that inherits from list
+    """
+    def print_sorted(self):
+        """
+        Prints the list, but sorted (ascending sort)
+        """
+        for element in self:
+            if type(element) is not int:
+                raise TypeError("all the elements of the list will be int")
+        print(sorted(self))
