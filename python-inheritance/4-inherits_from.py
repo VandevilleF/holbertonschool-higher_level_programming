174ade5a7ef790bb7e76c6f8f1bdd60bdbb0c606
VandevilleF <flo.vandeville@gmail.com>
2024-02-12 19:26:51 +0100

    Add 4-inherits_from.py for task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+returns True if the object is an instance of a class that inherited
+(directly or indirectly) from the specified class
+
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if obj is an instance of a class that inherited
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
