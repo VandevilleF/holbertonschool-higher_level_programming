a7bd34689436d3080c4cf63fa33045d50540e105
VandevilleF <flo.vandeville@gmail.com>
2024-02-12 19:34:47 +0100

    Pycodestyle: do not compare types, for exact checks use 'is' / 'id not', for instance checks use 'isinstance()' Juste change '!=' by 'is not'

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,7 +10,7 @@ def inherits_from(obj, a_class):
     """
     Check if obj is an instance of a class that inherited
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
         return False
