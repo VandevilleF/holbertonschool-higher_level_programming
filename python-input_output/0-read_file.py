3ccb44cf30302b1e232957fe49d2c9d9727645cc
VandevilleF <flo.vandeville@gmail.com>
2024-02-13 12:22:35 +0100

    Add 0-read_file.py for task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+Reads a text file (UTF8) and prints it to stdout
+
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end="")
