0b90d296dd2bc2e394218b01cc8ad768262554d5
VandevilleF <flo.vandeville@gmail.com>
2024-02-13 10:04:55 +0100

    Change bloc of classe BaseGeometry by a import of the task 7

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,18 +6,8 @@ A class BaseGeometry
 """
 
 
-class BaseGeometry:
-    """
-    A class BaseGeometry
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """
