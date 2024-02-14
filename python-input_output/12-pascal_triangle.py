d653163096d5b99e4dc9faa72074c3492c900b7c
VandevilleF <flo.vandeville@gmail.com>
2024-02-14 16:36:54 +0100

    Add block of comment under def function

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,6 +7,9 @@ of integers representing the Pascal's triangle of n
 
 
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers
+    representing the Pascal's triangle of n"""
 
     if n <= 0:
         return []
