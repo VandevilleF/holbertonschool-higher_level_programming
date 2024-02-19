6e006b5a12b8ccaf87323489a2b730dc0d441e17
VandevilleF <flo.vandeville@gmail.com>
2024-02-19 10:15:10 +0100

    Add base.py and __init__.py for task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+
+Class Base
+
+"""
+
+
+class Base:
+    """
+    Class Base
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+
+        if id == None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
