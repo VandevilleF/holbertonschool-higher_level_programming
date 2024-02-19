1d206a55a3a46bd646f5fca9134159c24d95efe3
VandevilleF <flo.vandeville@gmail.com>
2024-02-19 11:23:20 +0100

    Add rectangle.py for task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+
+Class Rectangle inherits from Base
+
+"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Class Rectangle inherits from Base
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        return self.__height
+
+    @x.setter
+    def x(self, value):
+        return self.__x
+
+    @y.setter
+    def y(self, value):
+        return self.__y
