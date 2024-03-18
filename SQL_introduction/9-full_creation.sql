4ac2242b41aa86b3d9b91441a0b58037033abf6e
VandevilleF <flo.vandeville@gmail.com>
2024-03-18 10:04:02 +0100

    insert into on pultiple lines

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,9 +4,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 	name VARCHAR(256),
 	score INT
 );
-INSERT INTO second_table
-VALUE
-	(1, "John", 10),
-	(2, "Alex", 3),
-	(3, "Bob", 14),
-	(4, "Georges", 8);
+INSERT INTO second_table VALUE (1, "John", 10);
+INSERT INTO second_table VALUE (2, "Alex", 3);
+INSERT INTO second_table VALUE (3, "Bob", 14);
+INSERT INTO second_table VALUE (4, "Georges", 8);
