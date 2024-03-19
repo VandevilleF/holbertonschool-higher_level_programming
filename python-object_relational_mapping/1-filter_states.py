c028ab1797e91762c80cd6278a46b220f27dd8e7
VandevilleF <flo.vandeville@gmail.com>
2024-03-19 12:18:57 +0100

    Add BINARY after LIKE FOR a case-sensitive comparison

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            password=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%'
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
                 ORDER BY id ASC""")
     query_rows = cur.fetchall()
     for row in query_rows:
